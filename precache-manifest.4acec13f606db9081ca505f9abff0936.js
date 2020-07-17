7327640ad0cac77d6039d8bddaca20d753d3259b
ahemanth27 <hemanth27.achanta@gmail.com>
2020-07-17 13:58:16 +0530

    Updates

---
+++ precache-manifest.4acec13f606db9081ca505f9abff0936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bd1015a01063d037fb1117376aaeae",
+    "url": "/gpa-calculator/index.html"
+  },
+  {
+    "revision": "f96c5998b6f9a0607007",
+    "url": "/gpa-calculator/static/css/main.b7e0fc35.chunk.css"
+  },
+  {
+    "revision": "d02d8011ab232e65e30f",
+    "url": "/gpa-calculator/static/js/2.1c3fd946.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gpa-calculator/static/js/2.1c3fd946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96c5998b6f9a0607007",
+    "url": "/gpa-calculator/static/js/main.d328087d.chunk.js"
+  },
+  {
+    "revision": "d5479effeb6a7b3bd7e9",
+    "url": "/gpa-calculator/static/js/runtime-main.73fd3148.js"
+  }
+]);
