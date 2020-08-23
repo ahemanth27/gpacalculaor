9235082a4abc5e397db25fe24eb3ccc2b7011eab
ahemanth27 <hemanth27.achanta@gmail.com>
2020-08-23 23:21:20 +0530

    mobile message:

--- src/App.js
+++ src/App.js
@@ -2,34 +2,41 @@ import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 
 import TabPanel from "./Components/Tabs";
+import icon from "./static/phone.svg";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <div id="title">
-        <h1>GPA Calculator</h1>
-        <p>
-          Calculate your Semester GPA, Cumulative CGPA and Estimated Grade for a
-          subject
-        </p>
+    <>
+      <div className="App">
+        <div id="title">
+          <h1>GPA Calculator</h1>
+          <p>
+            Calculate your Semester GPA, Cumulative CGPA and Estimated Grade for
+            a subject
+          </p>
 
-        <TabPanel />
+          <TabPanel />
+        </div>
+        <div
+          className="footer"
+          style={{
+            textAlign: "center",
+            marginTop: "2em",
+            // position: "fixed",
+            // left: "50%",
+            // transform: "translate(-50%, 20%)",
+          }}
+        >
+          <p> Made with love exclusively for VITians &#128154;</p>
+        </div>
       </div>
-      <div
-        className="footer"
-        style={{
-          textAlign: "center",
-          marginTop: "2em",
-          // position: "fixed",
-          // left: "50%",
-          // transform: "translate(-50%, 20%)",
-        }}
-      >
-        <p> Made with love exclusively for VITians &#128154;</p>
+      <div className="phone">
+        <p>This website is not currently compatible with mobile</p>
+        <img src={icon} className="phoneIcon" />
       </div>
-    </div>
+    </>
   );
 };
 
